15c701d23a28790483f423f0214e0c09f3f25cde
Justin <lesliej@uni.coventry.ac.uk>
2016-03-04 00:12:07 +0000

    Update Views.py

--- views/Views.py
+++ views/Views.py
@@ -4,6 +4,13 @@ from engine.Engine import *
 
 green = (156, 219, 151)
 green_bright = (0,150,0)
+orange = (0, 255,255)
+white = (255, 255, 255)
+
+blankImage = pygame.image.load('Selection_screen.png') 
+
+def game_image(x, y):
+    gameDisplay.blit(blankImage, (x, y)
 
 class StartScreen():
     def __init__(self):
@@ -22,5 +29,49 @@ class StartScreen():
         self.objects.append(obj)
 
 class SelectionScreen():
-    def __init__(self):
-        pass
+    def game_select_items_menu():
+        global textDisp
+        
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        
+        gameSelectItems = False
+        white not gameSelectItems:
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                    
+            blank_image(x, y)
+            if (not textDisp is None):
+                
+                screen = pygame.display.get_surface()
+                textRect = textDisp.get_rect()
+                textRect.center = ((30+500/2)),(150+(350/2))
+                screen.blit(textDisp, textRect)
+                
+            button2("Xbox One", 374, 60, 168, 48, white, orange, 310, 32, "Xbox One")
+            
+            button2("Playstation 4", 200, 60, 168, 48, white, orange, 290, 29, "Playstation 4")
+            
+            button2("Kettle", 30, 60, 163, 48, white, orange, 70, 10, "Kettle")
+            
+            button2("Lewi Jeans", 200, 160, 163, 48, white, orange, 399, 41, "Samsung TV")
+            
+            button2("MacBook", 200, 160, 168, 48, white, orange, 689, 92, "MacBook")
+            
+            button2("Samsung TV", 30, 160, 163, 48, white, orange, 399, 41, "Samsung TV")
+            
+            button2("Nike Air Max", 374, 250, 168, 48, white, orange, 90, 10, "Nike Air Max")
+            
+            button2("Apple iPad", 200, 250, 168, 48, white, orange, 189, 30, "Apple iPad")
+            
+            button2(
+            
+            
+            
+            
+            
+        
+        
