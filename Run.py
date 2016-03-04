3d03779d0eef61038a3ee830a6be3c21fc02fb2a
Justin <lesliej@uni.coventry.ac.uk>
2016-03-04 02:21:29 +0000

    Update Run.py

--- Run.py
+++ Run.py
@@ -5,6 +5,7 @@ from views.Views import StartScreen
 # this will be moved to components soon
 mainBackground2 = pygame.image.load('assets/main_menu_background.png')
 blankImage = pygame.image.load('selection_screen.png')
+
 engine = GameEngine()
 engine.change_view(StartScreen())
 engine.main_loop()
