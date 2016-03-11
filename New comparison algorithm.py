8f06d92cbcdf3e0f7db135060a39d3b52e91f127
Lwalsh13 <walshl13@uni.coventry.ac.uk>
2016-03-11 20:20:37 +0000

    Canvas comparison

---
+++ New comparison algorithm.py
@@ -0,0 +1,27 @@
+
+class canvasComparsionLinear:
+    def linearSearch(canvasx,canvasy,desiredx,desiredy):
+        canvasx=[2]
+        canvasy=[2]
+        desiredx[0]
+        desiredy=[0]
+
+    while canvasx == True:
+        if canvasx > desiredx:
+            canvasx -= 1
+        elif canvasx < desiredx:
+            canvasx += 1
+        else:
+            print('X found')
+            canvasx = False 
+    while canvasy == True:
+        if canvasy > desiredy:
+            canvasy -= 1
+        elif canvasy < desiredy:
+            canvasy += 1
+        else:
+            print('Y found')
+            canvasy = False
+        
+    return ("searching")
+    
