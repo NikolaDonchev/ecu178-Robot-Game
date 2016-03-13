9dccf794b16f9adcbd0ce38e92bce631af221238
Mario Kostadinov <mmkostadinov@gmail.com>
2016-03-13 02:50:51 +0000

    Minor Changes in the code
    Some fixes

--- engine/engine.py
+++ engine/engine.py
@@ -194,16 +194,3 @@ class OptionsScreen():
         ]
 
         Engine().main_loop(self.objects)
-
-"""class MusicLoop():
-    def __init__(self):
-        self.display = Gui().display
-        self.objects = [
-            Background(self.display, menuBackground),
-            Button(self.display, "Music ON/OFF", 220, 280, 163, 48, green, green_bright, "OnOff"),
-            Button(self.display, "Volume +", 220, 340, 80, 48, green, green_bright, "+"),
-            Button(self.display, "Valume -", 303, 340, 80, 48, green, green_bright, "-"),
-            Button(self.display, "BACK", 220, 400, 163, 48, green, green_bright, "option")
-        ]
-
-        Engine().main_loop(self.objects)"""
